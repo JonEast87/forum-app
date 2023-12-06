687829053bbdc21dbba2a4beddca2e751bf7b6d8
JonEast87 <j.d.east@icloud.com>
2023-12-06 16:38:31 -0700

    added a bit of a hackish method to ensure my comps. are showing up correctly, will begin to assembly it like normal once all comps are done.

--- forum-client/src/App.tsx
+++ forum-client/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+// import leftMenu from "./components/LeftMenu";
 
 function App() {
   return (
+    // <Switch>
+    //   <Route exact={true} path="/" component={leftMenu} />
+    // </Switch>
     <div className="App">
       <nav className="navigation">Nav</nav>
       <div className="sidebar">Sidebar</div>
